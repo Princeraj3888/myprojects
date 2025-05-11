05ec37f00614ac93854b7f8352108853c7fe30cf
Princeraj3888 <luckyraj2307@gmail.com>
2025-05-11 16:47:05 +0700

    fdafs

---
+++ matching-engine/src/test/java/com/exploretech/MatchingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploretech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
